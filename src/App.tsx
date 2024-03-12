c65d9ea31347b5a05bff32cf5326ec6864d8f465
Alexandr Kostromin <alexkostromin9797@gmail.com>
2024-03-12 17:56:48 +0300

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
+import {Error404} from "./components/pages/Error404";
 
 
 function App() {
@@ -12,13 +13,18 @@ function App() {
                 <div className={styles.header}><h1>HEADER</h1></div>
                 <div className={styles.body}>
                     <div className={styles.nav}>
-
+                        <div><NavLink to={'page1'} className={({isActive})=> isActive ? styles.active : styles.navLink }>Page1</NavLink></div>
+                        <div><NavLink to={'page2'} className={({isActive})=> isActive ? styles.active : styles.navLink }>Page2</NavLink></div>
+                        <div><NavLink to={'page3'} className={({isActive})=> isActive ? styles.active : styles.navLink }>Page3</NavLink></div>
                     </div>
                     <div className={styles.content}>
                         <Routes>
+                            <Route  path='/' element={<Navigate to={'page1'}/>}/>
                             <Route  path='page1' element={<PageOne/>}/>
                             <Route  path='page2' element={<PageTwo/>}/>
                             <Route  path='page3' element={<PageThree/>}/>
+                            <Route  path='page/error' element={<Error404/>}/>
+                            <Route path={'/*'} element={<Navigate to={'page/error'}/>}/>
                         </Routes>
                     </div>
                 </div>
